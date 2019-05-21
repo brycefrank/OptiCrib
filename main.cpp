b5adb3d76da97e5cfaa5476077d768e4332cefe1
brycefrank <bryce.frank@oregonstate.edu>
2019-05-20 20:56:55 -0700

    implementing shuffling (got stuck...)

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <array>
 
 using namespace std;
 
@@ -26,32 +28,67 @@ class Card {
 
 };
 
-int main() {
-	Card c [52];
+class Deck {
+	public:
+		Card cards[52];
+
+		Deck() {
+			int i = 0;
+			for (int v=1; v <= 13; v++) {
+				for (int s=1; s<=4; s++) {
+					string b;
+					switch(s) {
+						case 1: b = "Hearts";
+						break;
+
+						case 2: b = "Spades";
+						break;
+
+						case 3: b = "Diamonds";
+						break;
+
+						case 4: b = "Clubs";
+						break;
+					};
+
+					Card card (v, b);
+					cards[i] = card;
+					i += 1;
+				}
+			}
+		}
+
+		Card getcard(int i) {
+			return cards[i];
+		}
+
+};
+
+// Returns random integers between 0 and 51 without replacement
+int * randomizeDeck() {
+	static int order[52];
 	int i = 0;
-	for (int v=1; v <= 13; v++) {
-		for (int s=1; s<=4; s++) {
-			string b;
-			switch(s) {
-				case 1: b = "Hearts";
-				break;
 
-				case 2: b = "Spades";
-				break;
+	while (i < 52) {
+		// Check if in order using find
+		int rand_num = rand() % 52;
+		int *finder = find(begin(order), end(order), rand_num);
 
-				case 3: b = "Diamonds";
-				break;
 
-				case 4: b = "Clubs";
-				break;
-			};
-
-			Card card (v, b);
-			c[i] = card;
+		if (finder == end(order)){
+			// The element was not found, append!
+			order[i] = rand_num;
 			i += 1;
+			cout << order[i];
 		}
 	}
-	cout << c[0].getvalue();
+
+	return order;
+}
+
+int main() {
+	Deck d;
+	randomizeDeck();
 	return 0;
 }
 
