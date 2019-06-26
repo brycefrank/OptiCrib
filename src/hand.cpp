50dc3d39493e854578bc0c2d1614776336d46476
brycefrank <bryce.frank@oregonstate.edu>
2019-06-26 13:56:26 -0700

    small laptop update

--- src/hand.cpp
+++ src/hand.cpp
@@ -1,9 +1,22 @@
 #include "hand.h"
 #include <algorithm>
+#include <iostream>
+#include <vector>
 
 Hand::Hand(){}
 
-int Hand::count_hand() {}
+
+int Hand::count_hand(Card stack_card) {
+    std::vector<int> vals(cards.size() + 1);
+
+    for (int i = 0; i < cards.size(); i++){
+        vals[i] = cards[i].get_crib_value();
+    }
+
+    int last_ix = vals.size() - 1;
+    vals[last_ix] = cards[last_ix].get_crib_value();
+    all_combinations(vals, 3);
+}
 
 void Hand::change_order(int old_ix, int new_ix) {
     std::iter_swap(cards.begin() + old_ix, cards.begin() + new_ix);
--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,26 @@
 #include "player.h"
 #include <iostream>
 
+#include "deck.h"
+#include "hand.h"
+
 int main() {
     setlocale(LC_ALL, "");
-    Controller c;
-    c.start();
-    int g = getch();
-    endwin();
+
+    Deck d;
+    Hand h;
+
+    d.shuffle();
+
+    for (int i = 0; i < 4; i++ ) {
+        d.transfer_card(i, h);
+    }
+
+    h.count_hand(d.cards.at(0));
+
+    //Controller c;
+    //c.start();
+    //int g = getch();
+    //endwin();
 	return 0;
 }
